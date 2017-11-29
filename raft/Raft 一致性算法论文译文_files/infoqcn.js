2133c04ad73278936916a46436ccef60ca8f843b
liangzhicong <zhicong.liang@lifesense.com>
2017-11-29 10:38:00 +0800

    add file $rename

--- raft/Raft 一致性算法论文译文_files/infoqcn.js
+++
@@ -1,8 +0,0 @@
-if(Math.random() < 0.25){$(".text_info .related").before('<div style=margin-bottom:5px"><a href="http://ai.baidu.com/meeting/page/form/9E89CB3D8BCC42999432D387F4D0FBED?channel=infoQ" target="_blank"><img  style="width: 100%;" src="http://static.geekbang.org/infoq/5a02bfbdab284.png" /></a></div>')
-console.log("请关注InfoQ微信帐号:infoqchina");}
-else if(Math.random() >= 0.25&&Math.random() < 0.5){$(".text_info .related").before('<div style=margin-bottom:5px"><a href="http://bj2017.archsummit.com/?utm_source=infoq&utm_medium=wzbottom" target="_blank"><img  style="width: 100%;" src="http://static.geekbang.org/infoq/59f68fbc11458.png" /></a></div>')
-console.log("请关注InfoQ微信帐号:infoqchina");}
-else if(Math.random() >= 0.5&&Math.random() < 0.75){$(".text_info .related").before('<div style=margin-bottom:5px"><a href="https://aicon.geekbang.org/?utm_source=InfoQ&utm_medium=wzbottom" target="_blank"><img  style="width: 100%;" src="http://static.geekbang.org/infoq/59f98aa56c635.png" /></a></div>')
-console.log("请关注InfoQ微信帐号:infoqchina");}
-else{$(".text_info .related").before('<div style=margin-bottom:5px"><a href="https://time.geekbang.org/column/intro/49?utm_source=website&utm_medium=infoq&utm_campaign=promotion&utm_content=zhuyun" target="_blank"><img  style="width: 100%;" src="http://static.geekbang.org/infoq/5a0948cebe0d2.png" /></a></div>')
-console.log("请关注InfoQ微信帐号:infoqchina");}
